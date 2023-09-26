8cafec0daacca0def3a7032516f9d99b94274788
tmar01 <145872758+tmar01@users.noreply.github.com>
2023-09-26 11:29:25 -0700

    Update streamlit_app.py
    Disable new display code

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response)
