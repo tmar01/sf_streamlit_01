5d19786515fb29888ab72633780b7311b555e011
tmar01 <145872758+tmar01@users.noreply.github.com>
2023-09-26 13:28:56 -0700

    Update streamlit_app.py
    Remove raw json display and separate url for variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ streamlit.dataframe(fruits_to_show)
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+# streamlit.text(fruityvice_response.json())
 
 # normalize json version of response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
