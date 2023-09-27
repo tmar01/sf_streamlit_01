e04c11b3a1125c346a3b725a6f0e09f590493720
tmar01 <145872758+tmar01@users.noreply.github.com>
2023-09-26 19:05:07 -0700

    Update streamlit_app.py
    Stop code execution before importing snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #work with snowflake connector
+# don’t run anything past here while we troubleshoot
+streamlit.stop()
 import snowflake.connector;
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
