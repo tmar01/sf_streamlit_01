9898f30b18f0300f5e6f5df0f906e5d768654ff2
tmar01 <145872758+tmar01@users.noreply.github.com>
2023-09-26 19:53:20 -0700

    Update streamlit_app.py
    semicolon at end of def line

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display fruits_to_show instead of the entire list
 streamlit.dataframe(fruits_to_show)
 
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
     #streamlit.write('The user entered ', fruit_choice)
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     # streamlit.text(fruityvice_response.json())
