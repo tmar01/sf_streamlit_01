fff6ed6872cc895391c9ddf650573424214efa30
tmar01 <145872758+tmar01@users.noreply.github.com>
2023-09-26 21:03:18 -0700

    Update streamlit_app.py
    Add exclamation point

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ except URLError as e:
     streamlit.error()
 
 # fruit load list
-streamlit.header("View Our Fruit List - Add Your Favorites")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
