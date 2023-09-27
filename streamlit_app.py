7d4ea4f9d0e9d0d7e0092c9bfa8a8da515087f42
tmar01 <145872758+tmar01@users.noreply.github.com>
2023-09-26 19:30:22 -0700

    Update streamlit_app.py
    Don't preload kiwi as default

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   # get user input
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
